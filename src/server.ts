b3960ae79b63dfaffda2c719eb295902f09a4ed8
Michele Acierno <michele.m.acieno@gmail.com>
2020-03-03 18:05:37 +0100

    Adding server

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import Tarrot from "./services/Tarot";
 import Esagrams from "./services/Esagrams";
 import TelegramBot from "node-telegram-bot-api";
+import express from "express";
+import path from "path";
+import http from "http";
 
 // replace the value below with the Telegram token you receive from @BotFather
 // @ts-ignore
@@ -10,4 +13,14 @@ const bot = new TelegramBot(token, { polling: true });
 Esagrams.init(bot);
 Tarrot.init(bot);
 
-console.log("Server Started");
+
+const app = express();
+const server = new http.Server(app);
+
+const port = process.env.PORT || 8080;
+
+app.use('/', express.static(path.join(__dirname, 'testheroku')));
+
+server.listen(port, () => {
+    console.log(`Listening on http://localhost:${port}/`);
+});
