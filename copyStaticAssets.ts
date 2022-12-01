b9788acea5ddc2535bc6147cf8626723f6b45dc5
Michele Acierno <michele.m.acierno@gmail.com>
2022-12-01 11:13:50 +0100

    Create dist

--- copyStaticAssets.ts
+++ copyStaticAssets.ts
@@ -1,6 +1,7 @@
 import * as shell from "shelljs";
 
 async function createDirs(){
+    await shell.mkdir("dist");
     await shell.mkdir("dist/services/");
     shell.cp("-R", "src/services/fonts/", "dist/services/");
     shell.cp("-R", "src/services/tarots/", "dist/services/");
